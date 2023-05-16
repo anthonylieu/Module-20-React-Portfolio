224de7264651b5821ef7bdc34edb34b0e54f5844
anthonylieu <38957648+anthonylieu@users.noreply.github.com>
2023-05-16 03:44:55 -0400

    tested functionality

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot, hydrateRoot } from 'react-dom/client';
 import App from './App';
 
-// See https://reactjs.org/docs/strict-mode.html
 const StrictApp = () => (
   <React.StrictMode>
     <App />
@@ -11,7 +10,6 @@ const StrictApp = () => (
 
 const rootElement = document.getElementById('root');
 
-// hydrate is required by react-snap.
 if (rootElement.hasChildNodes()) {
   hydrateRoot(rootElement, <StrictApp />);
 } else {
